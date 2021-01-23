990072d9b158232759a327de5fdcd29c1004b08f
otsuka-haruki <for.business.in.general@gmail.com>
2021-01-24 01:02:54 +0900

    works and cover animation

--- assets/scripts/cover.js
+++ assets/scripts/cover.js
@@ -3,12 +3,12 @@ export function coverAnimation() {
   const coverElement = document.getElementById('cover');
   const coverPhrase = coverElement.querySelector('p');
   coverPhrase.textContent = 'loading...';
-  
+
   setTimeout(() => {
     coverPhrase.textContent = 'loaded !';
   }, 2500);
 
   setTimeout(() => {
-    coverElement.classList.add('upDisapper');
+    coverElement.classList.add('bounce-up');
   }, 3500);
 }
