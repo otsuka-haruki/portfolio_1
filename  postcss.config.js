7ff0f7f0196533a0353216a9869f17133efd426e
otsuka-haruki <for.business.in.general@gmail.com>
2021-01-24 03:09:40 +0900

    post css and 100vh?

---
+++  postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: [
+    require('precss'),
+    require('postcss-100vh-fix'),
+    require('autoprefixer')
+  ]
+}
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ]
+      },
+    ],
+  },
+};
